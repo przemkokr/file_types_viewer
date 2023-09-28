31084f02ef23ec5658185e2dd09fdaa1712bfb20
Przemysław Król <pkr@unik.dk>
2023-09-28 16:59:55 +0200

    word viewer - troubles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { RtfViewerComponent } from './viewers/rtf-viewer/rtf-viewer/rtf-viewer.component';
 import { RtfService } from './viewers/rtf-viewer/rtf-viewer/rtf-serv.service';
--- src/app/viewers/ms-apps-viewer/word-viewer/word-viewer.component.ts
+++ src/app/viewers/ms-apps-viewer/word-viewer/word-viewer.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-word-viewer',
-  templateUrl: './word-viewer.component.html',
-  styleUrls: ['./word-viewer.component.scss']
+  template: `
+    <iframe [src]="docUrl" style="width:100%; height:600px;" frameborder="0"></iframe>
+  `
 })
 export class WordViewerComponent implements OnInit {
-
-  constructor() { }
-
   ngOnInit(): void {
+    console.log('oninit');
+    this.docUrl = this.generateGoogleDocsUrl(this.docPath);
   }
 
+  constructor(private sanitizer: DomSanitizer) {
+    this.docUrl = this.generateGoogleDocsUrl(this.docPath);
+  }
+
+  docPath: string = 'assets/sample_word.docx';
+  docUrl: SafeResourceUrl = '';
+
+  generateGoogleDocsUrl(path: string): SafeResourceUrl  {
+    const url = `https://docs.google.com/gview?url=${location.origin}/${path}&embedded=true`;
+    console.log(url);
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
 }
